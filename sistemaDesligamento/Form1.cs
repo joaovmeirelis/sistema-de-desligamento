a7ea7d4503aad03d808efcd886bd7457049be95a
joaovmeirelis <vitormeirelissilva@gmail.com>
2024-10-15 20:15:08 -0300

    adicionei Biblioteca

--- sistemaDesligamento/Form1.cs
+++ sistemaDesligamento/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics; //Biblioteca para Diagnostico do sistema
 
 namespace sistemaDesligamento
 {
